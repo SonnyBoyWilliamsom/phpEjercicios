31ec179ed9154be26fc220ca9e2248d348511e7e
cice <cice@AULA3POV03.CICE.LOCAL>
2015-05-22 09:58:24 +0200

    viernesss

--- 009_3_condicionales_form.php
+++ 009_3_condicionales_form.php
@@ -1,85 +1,171 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-       
+        
         <h2>Validación de campos de un formulario</h2>
-        
-        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-            <label>Email</label>
-            <input type="email" name="email">
-            <label>Password</label>
-            <input type="password" name="pass">
+        <!--Formulario con campo email & password procesado por el mismo documento -->
+        <fieldset>
+            <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="get">
+           
+            <label>User email: </label>
+            <input name="UserEmail" placeholder="email" type="email"><br><br>
+            <label>Password: </label>
+            <input name="UserPass" placeholder="Pass" type="text"><br><br>
+            <input type="submit" value="Send">
             
-            <input type="submit" value="Entrar">
         </form>
-        
-        <?php
-        //Al ejecutar el ejercicio por primera vez no se ha enviado información, por tanto no llega ninguna información del método get, sólo cuando le damos a enviar, se vuelve a ejecutar el documento y se vuelve a preguntar si existe información procedente del formulario, cómo ahora si llegan datos, se cumple la condición del isset y se pregunta por si esos datos vienen vacios o no con empty
-        if(isset($_GET["email"])){
-            if( empty($_GET["email"]) || empty($_GET["pass"]) ){
-                echo "Debes rellenar los datos";
-            }else{
-                echo "Login correcto";
+        </fieldset>
+        <div>
+            <?php
+            if($_GET){
+                extract($_GET);
+                if( empty($UserEmail)&&!empty($UserPass)){
+                    echo 'Please enter Email';
+
+                }else if(empty($UserPass)&&!empty($UserEmail)){
+                     echo 'Please enter Password';
+                }else if(empty($UserPass)&& empty($UserEmail)){
+                     echo 'Please fill the form';
+                }
+                else{
+                     echo 'Login Correct';
+                }
             }
-        }
-        ?>
+            ?>
+       </div>
         
-        <!---------------------------------------------------------------->
-        
-        <h2>Verificación de user y password</h2>
-        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
-            <label>Email</label>
-            <input type="email" name="email" required>
-            <label>Password</label>
-            <input type="password" name="pass" required>
+         <h2>Verificación de campos de un formulario</h2>
+        <!--Formulario con campo email & password procesado por el mismo documento -->
+         <fieldset>
+            <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+           
+            <label>User email: </label>
+            <input name="UserEmail" placeholder="email" type="email" required>*<br><br>
+            <label>Password: </label>
+            <input name="UserPass" placeholder="Pass" type="text" required>*<br><br>
+            <input type="submit" value="Send">
             
-            <input type="submit" value="Entrar">
         </form>
-        <?php
-        $user_perm=array("elena@cice.es","kike@cice.es");
-        $pass_perm=array("0000","1234");
-        
-        extract($_POST);
-        if($_POST){
-            if( ( ($email==$user_perm[0]) && ($pass==$pass_perm[0]) ) || ( ($email==$user_perm[1]) && ($pass==$pass_perm[1]) ) ){
-                echo "Login correcto";
-            }else{
-                echo "Usuario incorrecto";
-            }
-        }
-        
-        ?>
-        
+             
+        </fieldset>
+        <div>
+            <?php
+            if($_POST){
+                $user_perm=array("elena@cice.es","kike@cice.es"); 
+                $pass_perm=array("0000","1234");
+                extract($_POST);
+                if(($UserEmail==$user_perm[0] && $UserPass==$pass_perm[0])||($UserEmail==$user_perm[1] && $UserPass==$pass_perm[1]) ){
+                    echo 'Login Correct';
+                }else{
+                    echo 'Login Incorrect';
+                }
+           }
+          
+           ?>
+       </div>
         <!-------------------------------------------------------------------------------->
         
         <h2>Formulario con checkbox</h2>
         <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
             <input type="text" name="nombre" placeholder="Nombre">
             <input type="checkbox" name="edad">Soy mayor de edad
-            <input type="submit" value="Entrar">
+            <input type="submit" value="Entrar" name="form2">
         </form>
         <?php
         //Debes indicar tu nombre
         //Eres menor de edad y no puedes pasar
         //Bienvenid@ Pablo
-        if($_GET){
+        if(isset($_GET["form2"])){
            
             extract($_GET);
             //echo empty($edad);
-            if($edad=="on" && !empty($nombre)){//si se especifica edad y nombre
+            if(isset($edad) && !empty($nombre)){//si se especifica edad y nombre
                 echo 'Hola '.$nombre.'!!';
-            }else if(($edad=="on" && empty($nombre)) || (empty ($edad)&& empty($nombre))){//si se especifica edad o no pero no nombre
+            }else if(empty($nombre)){//si se especifica edad o no pero no nombre
                 echo 'Debes indicar tu nombre';
-            }else if(empty ($edad) && !empty($nombre)){
+            }else{
                 echo 'Eres menor de edad, no puedes pasar!';
             }
         }
         ?>
         
+        <!-------------------------------------------------------------------------------->
         
+        <h2>Validación de extensión</h2>
+        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
+            <input type="text" name="archivo" placeholder="fichero">
+           
+            <input type="submit" value="Send" name="form3">
+        </form>
+        
+        <?php
+        //Función X = Devuelve true si el valor buscado se encuentra en el array
+        $ext_perm=array("jpg","png");
+        
+        if(isset($_GET["form3"])){
+        extract($_GET);
+       // var_dump($_GET);
+            if(in_array(ltrim(strrchr($archivo, "."),"."),$ext_perm)){
+                echo 'FICHERO PERMITIDO';
+            }else{
+                echo 'FICHERO NO PERMITIDO';
+            }
+        }
+        
+//        //Otra manera con explode y end
+//        if(isset($_GET["form3"])){
+//        extract($_GET);
+//        var_dump($_GET);
+//        $a=explode( ".",$archivo);
+//            if(in_array(end($a),$ext_perm)){
+//                echo 'FICHERO PERMITIDO';
+//            }else{
+//                echo 'FICHERO NO PERMITIDO';
+//            }
+//        }
+        
+        
+        ?>
+        
+        <!-------------------------------------------------------------------------------->
+        
+        <h2>Formulario con radiobutton</h2>
+        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
+            <input type="text" name="nombre" placeholder="Nombre" required>*
+            <input type="number" name="edad" placeholder="edad" required>*
+            <input type="radio" name="sexo" value="mujer" checked> F
+            <input type="radio" name="sexo" value="hombre"> M
+            
+            <input type="submit" value="Send" name="form4">
+        </form>
+        
+        <?php
+        //Eres menor NOMBRE
+        //Bienvenido/Bienvenida
+        
+        if(isset($_GET["form4"])){
+            
+            extract($_GET);
+            $edadMax=18;
+            if($edad<$edadMax){
+                echo 'Eres menor '.$nombre;
+                
+            }else if($sexo=="mujer"){
+                echo 'Bienvenida'.$nombre;
+            }else{
+
+                echo 'Bienvenido'.$nombre;
+            }       
+        }
+        ?>
     </body>
 </html>
