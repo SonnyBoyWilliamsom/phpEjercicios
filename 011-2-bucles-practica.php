8fea6db8480cbf5e9a480b484d637bd640546cf4
cice <cice@AULA3POV03.CICE.LOCAL>
2015-06-02 11:34:39 +0200

    foreach bucles

--- 011-2-bucles-practica.php
+++ 011-2-bucles-practica.php
@@ -22,7 +22,8 @@ and open the template in the editor.
            
         <?php } ?>
         </ul>
-            
+        
+        <!------------------------------------------------>    
         <h3>FOR</h3>
          <ul>
         <?php for($i=0; $i<count($colores);$i++){ ?>
@@ -30,7 +31,7 @@ and open the template in the editor.
            
         <?php } ?>
         </ul>
-        
+         <!------------------------------------------------>
         <h3>FOREACH</h3>
          <ul>
         <?php foreach($colores as $indice=>$color){ ?>
@@ -47,5 +48,50 @@ and open the template in the editor.
         echo rtrim($lista_telfs,"* ");
         ?>
       
-    </body>
-</html>
+          <!------------------------------------------------>
+          <h3>Bucle for</h3>
+          <?php
+          $numeros="";
+          for($i=0;$i<9;$i++){
+            $numeros .= $i."-";
+            }
+            echo rtrim($numeros,"-");
+          ?>
+          
+          <!------------------------------------------------>
+          <h3>Bucle While</h3>
+          <?php 
+        $i=5;
+        $numeroBack ="";
+        while($i>0){ 
+            $numeroBack .= $i--."*";
+        }
+        echo rtrim($numeroBack,"*");
+        ?>
+          
+           <!------------------------------------------------>
+         <h3>Suma con un array asociativo</h3>   
+        <?php 
+        $numeros=array("n1"=>1,"n2"=>10,"n3"=>2,"n4"=>6);
+        //1+10+2
+        $suma=0;
+        $valores="";
+        foreach ($numeros as $indice => $numero) {
+            $suma+=$numero;
+            $valores.=$numero."+";
+        }
+        echo rtrim($valores,"+")."=".$suma;
+        ?>
+         
+         <!------------------------------------------------>
+         <h3>Valores pares hasta 100</h3>   
+        <?php 
+      
+        for($i=0;$i<=100;$i++){
+            if(!($i%2)){
+                echo $i." ";
+            }
+        }
+        ?>
+         
+         
