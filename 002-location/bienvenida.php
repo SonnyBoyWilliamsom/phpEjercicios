52bb14dbf28a1993dc80e79170c9b567aabd81a1
cice <cice@AULA3POV03.CICE.LOCAL>
2015-06-11 11:41:01 +0200

    rescate

---
+++ 002-location/bienvenida.php
@@ -0,0 +1,12 @@
+<?php var_dump($_GET); ?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Bienvenid@ <?= "eooo" ?> </h1>
+    </body>
+</html>
