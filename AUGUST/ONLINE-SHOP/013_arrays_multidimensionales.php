e8e96bcbace6a45db571719a0c16a52bdb698154
SonnyBoyWilliamsom <SonnyBoyWilliamsom@users.noreply.github.com>
2015-07-21 10:19:44 +0200

    Update 013_arrays_multidimensionales.php

--- AUGUST/ONLINE-SHOP/013_arrays_multidimensionales.php
+++ AUGUST/ONLINE-SHOP/013_arrays_multidimensionales.php
@@ -25,26 +25,6 @@
                 echo $valor." ";
             }
         }
-          <h2>Array multidimensional</h2>
-        <?php
-        $users= array( 
-                      array("nombre"=>"Paco","apellido"=>"Gomez"),
-                      array("nombre"=>"Pepe","apellido"=>"Flores"),
-                      array("nombre"=>"Jaime","apellido"=>"Col") 
-                    );
-        //var_dump($users);
-        //echo $users[0]["nombre"]." ".$users[0]["apellido"];
-        
-        for($i=0;$i<count($users);$i++){
-            echo $users[$i]["nombre"]." ".$users[$i]["apellido"]."<br>";
-        }
-        //////////////////////////////////////////////
-        foreach($users as $key=>$user){
-            foreach($user as $key=>$valor){
-                echo $valor." ";
-            }
-        }
-        
         ?>
         
         
